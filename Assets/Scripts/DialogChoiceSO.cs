00661127f07a7c228a9bfa1dab7b60f3fabd1986
flexinja99 <hjgo927@naver.com>
2026-03-25 10:33:42 +0900

    1교시 다이아로그 수업

---
+++ Assets/Scripts/DialogChoiceSO.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DialogChoiceSO", menuName = "Dialog System/DialogChoiceSO")]
+
+
+public class DialogChoiceSO : ScriptableObject
+{
+    public string text;
+    public int nextId;
+    
+}
---
+++ Assets/Scripts/DialogDatabaseSO.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+
+[CreateAssetMenu(fileName = "DialogDatabaseSO", menuName = "Dialog System/DialogDatabaseSO")]
+
+
+public class DialogDatabaseSO : ScriptableObject
+{
+    public List<DialogSO> dialogs = new List<DialogSO>();
+
+    private Dictionary<int, DialogSO> dialogsByld;
+
+
+    public void Initailize()
+    {
+        dialogsByld = new Dictionary<int, DialogSO>();
+
+        foreach(var dialog in dialogs)
+        {
+            if(dialog != null)
+            {
+                dialogsByld[dialog.id] = dialog;
+            }
+        }
+    }
+
+    public DialogSO GetDialogByld(int id)
+    {
+        if (dialogsByld == null)
+            Initailize();
+
+        if(dialogsByld.TryGetValue(id, out DialogSO dialog))
+        {
+            return dialog;
+        }
+
+        return null;
+    }
+}
